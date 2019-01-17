85575bb43895045b02bc49324a7108d758752433
Brendan Herger <13herger@gmail.com>
2019-01-16 18:59:44 -0800

    Adding tests

---
+++ tests/testBinarySearchTree.py
@@ -0,0 +1,63 @@
+import unittest
+
+from interfaces.BinarySearchTree import BinarySearchTree
+
+
+class TestASDF(unittest.TestCase):
+
+    def test_api(self):
+        data_structure = BinarySearchTree()
+        required_methods = ['add', 'contains', 'remove', 'in_order_traversal']
+        for required_method in required_methods:
+            self.assertTrue(hasattr(data_structure, required_method))
+
+    def test1(self):
+        data_structure = BinarySearchTree()
+        self.assertFalse(data_structure.contains(17))
+        data_structure.add(17)
+        self.assertTrue(data_structure.contains(17))
+
+        data_structure.add(19)
+        data_structure.add(23)
+        self.assertTrue(data_structure.contains(17))
+        self.assertTrue(data_structure.contains(19))
+        self.assertTrue(data_structure.contains(23))
+
+        data_structure.remove(23)
+        self.assertFalse(data_structure.contains(23))
+        self.assertTrue(data_structure.contains(17))
+        self.assertTrue(data_structure.contains(19))
+        pass
+
+    def test2(self):
+        data_structure = BinarySearchTree()
+
+        sample_data = [124, 123, 451351, -234, 124, 351235, 51235, 51325, 3523512351235, -213512351]
+        sample_data_sorted = sorted(sample_data)
+
+        for sample_element in sample_data:
+            data_structure.add(sample_element)
+
+        in_order_traversal = data_structure.in_order_traversal()
+        self.assertListEqual(sample_data_sorted, in_order_traversal)
+        pass
+
+    def test3(self):
+        data_structure = BinarySearchTree()
+
+        data_structure.add(15)
+        data_structure.remove(15)
+        data_structure.add(15)
+        data_structure.add(15)
+        data_structure.remove(15)
+        data_structure.add(23)
+        data_structure.add(92)
+        data_structure.add(-1)
+        data_structure.add(-12)
+        data_structure.remove(23)
+
+        expected_result = sorted([15, 92, -1, -12])
+        in_order_traversal = data_structure.in_order_traversal()
+        self.assertListEqual(expected_result, in_order_traversal)
+
+        self.assertRaises(ValueError, data_structure.remove, 42)

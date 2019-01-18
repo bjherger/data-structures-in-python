845540fd454f58267770f831bde6abda0a559922
Brendan Herger <13herger@gmail.com>
2019-01-18 10:41:52 -0800

    Adding linkedlist interface and tests

---
+++ interfaces/LinkedList.py
@@ -0,0 +1,59 @@
+import logging
+from functools import total_ordering
+
+
+class LinkedList(object):
+    """
+    A linked list implementation, with the following attributes
+
+     - Singly linked
+     - First in, first out
+     - New elements added to end of list
+     - Elements removed from beginning of list
+
+    API roughly based on https://docs.oracle.com/javase/7/docs/api/java/util/LinkedList.html
+
+    """
+
+    def __init__(self):
+        pass
+
+    def add(self, element):
+        """
+        Add the element to the end of the list
+        :param element: An element to be added
+        :return: self
+        :rtype: LinkedList
+        """
+        pass
+
+    def remove(self):
+        """
+        Remove the element from the head of the list
+        :return: self
+        :rtype: LinkedList
+        """
+        pass
+
+    def peak(self):
+        """
+        Return the value of the head of the list, without removing it from the list
+        :return: Element at the head of the list.
+        """
+        pass
+
+    def contains(self, query_element):
+        """
+        Determine if the query element is in the linked list
+        :param query_element: An element to check for in the data structure
+        :return: A boolean, indicating whether the query element occurs in the data structure at least once
+        :rtype: bool
+        """
+
+    def size(self):
+        """
+        Returns the number of elements in the list
+        :return:
+        """
+        pass
+
---
+++ tests/testLinkedList.py
@@ -0,0 +1,77 @@
+import logging
+import unittest
+
+from interfaces.LinkedList import LinkedList
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+class TestBinarySearchTree(unittest.TestCase):
+
+    def test_api(self):
+        data_structure = LinkedList()
+        required_methods = ['add', 'remove', 'peak', 'contains', 'size']
+        for required_method in required_methods:
+            self.assertTrue(hasattr(data_structure, required_method))
+
+    def test0(self):
+        data_structure = LinkedList()
+        data_structure.add(12)
+        data_structure.add(6)
+        data_structure.add(18)
+        removed = data_structure.remove()
+
+        self.assertTrue(data_structure.contains(6))
+        self.assertTrue(data_structure.contains(18))
+        self.assertFalse(data_structure.contains(12))
+        self.assertEqual(12, removed)
+
+    def test1(self):
+        data_structure = LinkedList()
+        data_structure.add(12)
+        data_structure.add(12)
+        removed = data_structure.remove()
+
+        self.assertTrue(data_structure.contains(12))
+        self.assertEqual(12, removed)
+
+
+    def test2(self):
+        data_structure = LinkedList()
+        data_structure.add(17)
+        self.assertTrue(data_structure.contains(17))
+
+    def test3(self):
+        data_structure = LinkedList()
+        self.assertFalse(data_structure.contains(17))
+        data_structure.add(17)
+        self.assertTrue(data_structure.contains(17))
+
+        data_structure.add(19)
+        data_structure.add(23)
+        self.assertTrue(data_structure.contains(17))
+        self.assertTrue(data_structure.contains(19))
+        self.assertTrue(data_structure.contains(23))
+
+        removed = data_structure.remove()
+        self.assertEqual(17, removed)
+        self.assertTrue(data_structure.contains(19))
+        self.assertFalse(data_structure.contains(23))
+
+    def test4(self):
+        data_structure = LinkedList()
+
+        sample_data = [124, 123, 451351, -234, 124, 351235, 51235, 51325, 3523512351235, -213512351]
+        sample_data_sorted = sorted(sample_data)
+        for sample_element in sample_data:
+            data_structure.add(sample_element)
+
+        self.assertEqual(len(sample_element), data_structure.size())
+
+        for sample_element in sample_data_sorted:
+            self.assertTrue(data_structure.contains(sample_element))
+
+        for sample_element in sample_data:
+            self.assertTrue(data_structure.contains(sample_element))
+
+            removed = data_structure.remove()
+            self.assertEqual(sample_element, removed)
